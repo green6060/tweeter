ac6a9375ad925a60481adb2a1d35ff470c78a41b
tchoutdara <monkeymilitia444@gmail.com>
2018-09-08 16:10:10 -0600

    update uploader

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,7 @@ class Post < ApplicationRecord
   belongs_to :user
 
   has_many :comments
+
+  has_one_attached :main_picture # one-to-one relationship
+  has_many_attached :uploads # one-to-many relationship
 end
