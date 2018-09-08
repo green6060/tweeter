a35de1db2e441c155a50f8395a2eab3ef38ee842
parkerprecisionweb <dallasjamesparker@gmail.com>
2018-09-08 12:19:26 -0600

    add css / updating bio views

---
+++ app/assets/javascripts/main.js
@@ -0,0 +1,8 @@
+// When "Edit" button, of Bio "Show" form is pressed, remove "Show" form, and replace with "Edit" form
+//// Event Listner, for Button click, 
+//// followed by Remove old form, and insert new form
+
+
+// When "Submit" button, of Bio "Edit" form is pressed remove "Edit" form, and replace with "Show" form
+//// Event Listner, for Button click, 
+//// followed by Remove old form, and insert new form
