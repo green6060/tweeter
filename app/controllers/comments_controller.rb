accccd0805069d8feff4a2ab4df69095e824fa06
tprax <tprax01@gmail.com>
2018-09-08 13:15:51 -0600

    comments controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,16 +1,51 @@
 class CommentsController < ApplicationController
+  before_action :set_comment, only: [:show, :edit, :destroy]
   def index
+    @comments = Comment.all
   end
 
   def show
+
   end
 
   def new
+    @comment = Comment.new
+    render partial: 'form'
+  end
+
+  def create
+    @comment = Comment.new(comment_params)
+
+    if @comment.save
+      redirect_to post_comments_path(post.id)
+    else
+      render :new
+    end
   end
 
   def edit
+    
+  end
+
+  def update
+    if @comment.update(comment_params)
+      redirect_to post_comments_path(post.id)
+    else
+      render :edit
+    end
   end
 
   def destroy
+
+  end 
+
+  private
+  def set_comment
+    @comment = Comment.find(params[:id])
   end
+
+  def comment_params
+    params.require(:comment).permit(:message, :post_id)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,23 @@
 Rails.application.routes.draw do
-  devise_for :users
-
-  root 'posts#index'
-
-  resources :users do
-    resources :bios
-  end
-
-  resources :users do
+  devise_for :users do
     resources :posts
   end
 
-  resources :users do
+  root 'posts#index'
+
+  resources :posts do
     resources :comments
   end
+
+  resources :bio
+
+
+
+  # resources :users do
+  #   resources :posts do
+  #     resources :comments
+  #   end
+  # end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
